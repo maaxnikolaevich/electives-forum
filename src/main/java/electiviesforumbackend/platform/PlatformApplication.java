7b1669a751b694bebc139d0db92727fedc735497
Maks <lebedkom03@gmail.com>
2022-06-10 00:44:35 +0500

    add cors

--- src/main/java/electiviesforumbackend/platform/PlatformApplication.java
+++ src/main/java/electiviesforumbackend/platform/PlatformApplication.java
@@ -2,10 +2,23 @@ package electiviesforumbackend.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "electiviesforumbackend.platform")
 public class PlatformApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlatformApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("https://electives-forum-backend.herokuapp.com");
+			}
+		};
 	}
 }
